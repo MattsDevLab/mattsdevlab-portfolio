9f07b2105c26985e54eda95937316804275ba6c2
MattsDevLab <116913535+MattsDevLab@users.noreply.github.com>
2026-03-29 20:51:10 +0100

    update content on hero banner

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -4,8 +4,8 @@ export default function Hero(){
     return(
         <section className="hero">
             <div className="hero__inner">
-                <p className="hero__eyebrow">Frontend Developer / Accessibility / Mordern Web</p>
-                <h1 className="hero__title">Senior web developer building accessible, scalable digital experiences</h1>
+                <p className="hero__eyebrow">Accessibility / Scalable UI & CMS Systems</p>
+                <h1 className="hero__title">Senior Frontend Engineer.</h1>
                 <p className="hero__text">I specialise in accessibility-first frontend evelopment, scalable UI Architecture, and custom website builds - with deep experience in WordPress and growing experience in Next.js and React.</p>
             </div>
         </section>
